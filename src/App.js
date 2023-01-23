c7cd1db8f93116956863f2e448adeaf4ff735d5e
ShahanaShameer <shahanamalu.shameer@gmail.com>
2023-01-24 00:08:18 +0530

    counterapp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  //create a state=>counter-to hold value
+  const [counter,setCounter]=useState(0)
+  function incrementCounter(){
+    setCounter(counter+1)
+  }
+  function decrementCounter(){
+    if(counter!==0)
+    setCounter(counter-1)
+  }
+  function reset(){
+    setCounter(0)
+  }
+
   return (
-    <div className="App">
+    <div 
+    style={{
+      display:'flex',
+      flexDirection:'column',
+      alignItems:'center',
+      border:'2px solid gray' ,
+      margin:'20px',
+      padding:'200px',
+      
+    }} 
+    className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h3 className='h1'>Your Turn....!</h3>
+
+        
+       <h1
+       className='text-center'
+       >{counter}</h1>
+       <button className='b1'
+       style={{
+       
+        color:'white',
+        padding:'10px',
+        border:'2px solid gray',
+        margin:'4px'
+             }}
+       onClick={incrementCounter}>Add</button>
+       <button className='b2'
+       style={{
+        // backgroundColor:'red',
+        color:'white',
+        padding:'10px',
+        border:'2px solid gray',
+        margin:'4px'
+             }}
+       onClick={decrementCounter}>Reduce</button>
+       <button className='b3'
+       style={{
+        // backgroundColor:'black',
+        color:'white',
+        padding:'10px',
+        border:'2px solid gray',
+        margin:'4px'
+             }}
+       onClick={reset}>Reset</button>
       </header>
     </div>
   );
---
+++ src/components/Counterfun.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Counterfun() {
+  return (
+    <div>
+      Counterfun
+    </div>
+  )
+}
+
+export default Counterfun
